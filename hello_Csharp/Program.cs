569c7b86b038e034c2507893620d42995b0ee3ba
TeemoCaption <84553635+TeemoCaption@users.noreply.github.com>
2022-03-07 12:58:44 +0800

    加入專案檔案。

---
+++ hello_Csharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace hello_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double meal_cost = 100;
+            int tip_percent = 15;
+            int tax_percent = 8;
+            int total = ((int)meal_cost + tip_percent) * (1 + tax_percent);
+            Console.WriteLine($"總共是{total}");
+        }
+    }
+}
